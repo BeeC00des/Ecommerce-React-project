37c37ea68e1faf60bb37c25ee8cbe9ed40b24d8b
Ajayi bolarinwa <ajayibcd@gmail.com>
2022-02-16 05:20:29 -0800

    Updates

---
+++ precache-manifest.bae27a25612970c69e852d47a46a6b34.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7806ec5480a32fb01eaa6d05509aff5e",
+    "url": "/Ecommerce-React-project/index.html"
+  },
+  {
+    "revision": "2cbcfcdb3e1c953c9666",
+    "url": "/Ecommerce-React-project/static/css/2.bd4493a5.chunk.css"
+  },
+  {
+    "revision": "d9f6fab14099fe7e077f",
+    "url": "/Ecommerce-React-project/static/css/main.72999e69.chunk.css"
+  },
+  {
+    "revision": "2cbcfcdb3e1c953c9666",
+    "url": "/Ecommerce-React-project/static/js/2.06eb5174.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/Ecommerce-React-project/static/js/2.06eb5174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9f6fab14099fe7e077f",
+    "url": "/Ecommerce-React-project/static/js/main.6f7e04a0.chunk.js"
+  },
+  {
+    "revision": "e1b08bace56b95d17ee9",
+    "url": "/Ecommerce-React-project/static/js/runtime-main.d97b643f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Ecommerce-React-project/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Ecommerce-React-project/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Ecommerce-React-project/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Ecommerce-React-project/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Ecommerce-React-project/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
